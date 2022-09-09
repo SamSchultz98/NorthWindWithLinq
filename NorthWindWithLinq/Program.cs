c9d76998e066850036c5f32379f48297f9b2204f
Sam Schultz <sam.schultz@outlook.com>
2022-09-09 12:03:09 -0400

    Reformatted the Console Write Line On the join

--- NorthWindWithLinq/Program.cs
+++ NorthWindWithLinq/Program.cs
@@ -12,14 +12,15 @@ var SupplierProd = from S in _context.Suppliers
                    join P in _context.Products
                    on S.SupplierId equals P.SupplierId
                    orderby S.CompanyName
-                   select new       //Select new: Pick a property from the table you are joining together
+                   select new       //Select new: Pick a property from the table you are joining together. Only way to pull from both tables
+                   //Could put select new, and then select P and Select S (This will pull all data from both)
                    {
                        Supplier = S.CompanyName, Product = P.ProductName        //Putting this as a new alias in oder to call later
                    };
 
 foreach (var S in SupplierProd)
 {
-    Console.WriteLine($"{S.Supplier} {S.Product}"); 
+    Console.WriteLine($"{S.Supplier,-40} | {S.Product}");     //If you didn't have alias, you would put S.Supplier or S.ProductName instead
 }
 /*
 var products = _context.Products.ToList();
